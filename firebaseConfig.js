fea2ee7de6e8b0ecec5e93a36e07c768b808194e
vishal shukla <vishaaxl@gmail.com>
2022-07-06 19:10:37 +0530

    patch: added env file

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyByfd3N01DMchSnWZT3dIoHyBZY83d41SI",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "vishaaxl-invoice-app.firebaseapp.com",
   projectId: "vishaaxl-invoice-app",
   storageBucket: "vishaaxl-invoice-app.appspot.com",
   messagingSenderId: "611781970237",
-  appId: "1:611781970237:web:2a8b5923dc966672db1d54",
+  appId: process.env.FIREBASE_API_ID,
   measurementId: "G-X2178G15C4",
 };
 
